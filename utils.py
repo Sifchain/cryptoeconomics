63332e88a9ffb228ff7c7cb307191d22aa016b73
Eden Au <35846351+edenau@users.noreply.github.com>
2021-04-07 03:52:20 +0100

    Update getUserAccReward

--- utils.py
+++ utils.py
@@ -1,33 +1,37 @@
 def getUserAccReward(data, addressOfInterest):
     """
-    A wrapper that processes data and gets user's accumulated reward 
+    A wrapper that processes data and gets user's accumulated reward
     args:
         data: raw data
         addressOfInterest: address of interest
     requires:
         the following functions
     returns:
-        userAccReward: user's accumulated reward (in ROWAN)
+        reserved: reward already set aside for the user, ready to be claimed any time (in ROWAN)
+        claimable: user's accumulated *unclaimed* reward (in ROWAN)
+        currentMultiplier: user's current multiplier
     """
     # constants
     constants = {'miningSeconds':4*30*86400, # 120 days
-                 'totalReward':30e6, # 30M 
-                 'epochSeconds':40*60} # 40 minutes
-    
+                 'totalReward':30e6, # 30M
+                 'epochSeconds':200*60, # 200 minutes
+                 'multiplierSeconds': 4*30*86400, # 120 days
+                 'multiplierBand': [1,4]} # from 1x to 4x
+
     # data route
-    data = data['data']['snapshots_testing'][0]['snapshot_data']
-    
+    data = data['data']['snapshots_mongo'][0]['snapshot_data']
+
     def dict2list(d: dict):
         """convert a dictionary to a list"""
         l = []
         for k, v in d.items():
             l.append(v)
         return l
-    
+
     def elementwisesum(listoflists):
         """element-wise summation of lists"""
         return [sum(x) for x in zip(*listoflists)]
-    
+
     def get_geyser_accmulated_reward(userSnapshots, list_userSnapshots, **kwargs):
         """
         Get user's accumulated reward under geyser liquidity mining
@@ -55,7 +59,7 @@ def getUserAccReward(data, addressOfInterest):
                 userEpochsSnapshots: a list of user's liquidity-epochs provided at diff snapshots (in USD-snapshot)
             """
             # initialise
-            user_memory = [] 
+            user_memory = []
             userEpochsSnapshots = []
 
             for i in range(len(userSnapshots)):
@@ -97,7 +101,7 @@ def getUserAccReward(data, addressOfInterest):
                 list_userEpochsSnapshots.append(get_userEpochsSnapshots(l))
             globalEpochsSnapshots = elementwisesum(list_userEpochsSnapshots)
             return globalEpochsSnapshots
-        
+
         def get_normal_accmulated_reward(userSnapshots, globalSnapshots, **kwargs):
             """
             Get user's accumulated reward under normal liquidity mining
@@ -120,38 +124,135 @@ def getUserAccReward(data, addressOfInterest):
             # sum(reward distributed pro-rata at each epoch)
             userAccReward = sum([userStaked / globalStaked * totalRewardPerEpoch for userStaked, globalStaked in zip(userSnapshots, globalSnapshots)])
             return userAccReward
-        
-        userAccReward = get_normal_accmulated_reward(userSnapshots=get_userEpochsSnapshots(userSnapshots), 
-                                                     globalSnapshots=get_globalEpochsSnapshots(list_userSnapshots), 
+
+        userAccReward = get_normal_accmulated_reward(userSnapshots=get_userEpochsSnapshots(userSnapshots),
+                                                     globalSnapshots=get_globalEpochsSnapshots(list_userSnapshots),
                                                      **kwargs)
         return userAccReward
 
+    def get_multiplier_record(userSnapshots, **kwargs):
+        """
+        Get user's multiplier record for all snapshots
+        args:
+            userSnapshots: a list of user's liquidity provided at diff snapshots
+        kwargs:
+            multiplierSeconds: the period of reaching max multiplier (in seconds)
+            epochSeconds: the period of an epoch (in seconds) as we take a snapshot per epoch
+            multiplierBand: min and max multiplier (list with length of 2)
+        returns:
+            multiplier_record: user's multiplier record
+        """
+        def compute_multiplier(oldSnapshot, newSnapshot, oldMultiplier=0, **kwargs):
+            multiplierSeconds, epochSeconds, multiplierBand = kwargs['multiplierSeconds'], kwargs['epochSeconds'], kwargs['multiplierBand']
+
+            assert oldSnapshot >= 0, 'oldSnapshot is negative'
+            assert newSnapshot >= 0, 'newSnapshot is negative'
+            assert oldMultiplier == 0 or multiplierBand[0] <= oldMultiplier <= multiplierBand[1], 'invalid oldMultiplier'
+
+            if newSnapshot == 0: # if no liquidity
+                return 0
+            elif newSnapshot-oldSnapshot > 0 and oldMultiplier == 0: # just added liquidity from none
+                return multiplierBand[0]
+            elif newSnapshot-oldSnapshot < 0: # just removed some (not all) liquidity
+                return multiplierBand[0]
+            else:
+                multiplierIncrement = (multiplierBand[1]-multiplierBand[0]) / multiplierSeconds * epochSeconds
+                return min(multiplierBand[1], oldMultiplier + multiplierIncrement)
+
+        multiplier_record = []
+        for i in range(len(userSnapshots)):
+            if i == 0:
+                multiplier = compute_multiplier(0, userSnapshots[i], oldMultiplier=0, **kwargs)
+            else:
+                multiplier = compute_multiplier(userSnapshots[i-1], userSnapshots[i], oldMultiplier=multiplier, **kwargs)
+            multiplier_record.append(multiplier)
+
+        assert len(multiplier_record) == len(userSnapshots), 'Wrong length for multiplier_record'
+        return multiplier_record
+
     import numpy as np
-    
+
     # for quick access
     addressList = list(data)
     tokenList = list(data[addressList[0]])
-    
+
     # sanity check so that tokenList is universal
     for addy in addressList:
-        assert tokenList == list(data[addy]), 'wrong token list' 
+        assert tokenList == list(data[addy]), 'wrong token list'
 
     # convert events into snapshots
-    snapshot = {} # 
+    snapshot = {} #
     for addy in addressList: # for each addy
         snapshotOfOneAddress = 0
         for token in tokenList: # aggregate USD values across all tokens
             snapshotOfOneAddress += np.cumsum(data[addy][token], dtype=float)# cumulative sum
         snapshot[addy] = snapshotOfOneAddress
-        
+
     # turn all negative numbers to 0
     for addy, l in snapshot.items():
         snapshot[addy] = [max(0,e) for e in l]
-        
+
     list_userSnapshots = dict2list(snapshot)
     globalSnapshots = elementwisesum(list_userSnapshots)
-    
-    userAccReward = get_geyser_accmulated_reward(userSnapshots=snapshot[addressOfInterest], 
-                                                 list_userSnapshots=list_userSnapshots, 
-                                                 **constants)
-    return userAccReward
+
+    # establish a user profile
+    profile = {'liquidity':snapshot[addressOfInterest],
+               'reward':{'reserved':0, 'burned':0}}
+    profile['multiplier'] = get_multiplier_record(userSnapshots=profile['liquidity'],
+                                                  **constants)
+
+    for i in range(1, len(profile['multiplier'])):
+
+        if  profile['multiplier'][i-1] > profile['multiplier'][i]: # if multiplier is going down, a claim is triggered
+            userAccRewardSoFar = get_geyser_accmulated_reward(userSnapshots=profile['liquidity'][:i],
+                                                              list_userSnapshots=[l[:i] for l in list_userSnapshots],
+                                                              **constants)
+
+            profile['reward']['reserved'] += (userAccRewardSoFar - profile['reward']['burned']) * profile['multiplier'][i-1] / constants['multiplierBand'][1]
+            profile['reward']['burned'] = userAccRewardSoFar
+
+    userAccRewardSoFar = get_geyser_accmulated_reward(userSnapshots=profile['liquidity'],
+                                                      list_userSnapshots=list_userSnapshots,
+                                                      **constants)
+    profile['reward']['claimable'] = (userAccRewardSoFar - profile['reward']['burned']) * profile['multiplier'][-1] / constants['multiplierBand'][1]
+
+    return profile['reward']['reserved'], profile['reward']['claimable'], profile['multiplier'][-1]
+
+
+def inspect_address(data, address, filtering=False, detail=True):
+    """
+    Debug tool, not needed for deployment
+    Inspect the activities of anddress
+    args:
+        data: raw data
+        address: address of interest
+        filtering: if True, only select addresses that removes liq before adding any (default False)
+        detail: if True, print out human-readable detail
+    """
+    d = data[address]
+    tokenList = list(d)
+
+    if filtering:
+        i = 0
+        while len(d[tokenList[0]]) > i:
+            min_, max_ = 0, 0
+            for token in tokenList:
+                min_ = min(min_, d[token][i])
+                max_ = max(max_, d[token][i])
+
+            if min_ < 0 and max_ <= 0:
+                break
+            elif max_ > 0:
+                return None
+            i += 1
+
+        print(address)
+    i = 0
+    while len(d[tokenList[0]]) > i:
+        for token in tokenList:
+            if d[token][i] != 0:
+                if detail:
+                    print(i, token, d[token][i])
+        i += 1
+    if detail:
+        print()
