ad5adc795e0bf4fa0331fa3769bcf07522638569
Aidan Musnitzky <musnit@gmail.com>
2021-04-30 21:11:04 -0400

    fix

--- liquidity-mining-rewards-apr-2020/frontend/src/Chart.js
+++ liquidity-mining-rewards-apr-2020/frontend/src/Chart.js
@@ -8,10 +8,25 @@ class Chart extends React.Component {
     this.myRef = React.createRef();
     this.dataset = [100, 200, 300, 400, 500];
   }
+
   componentDidMount() {
-    d3.select(this.myRef.current)
-      .append('p')
-      .text('Hello from D3');
+    let size = 500;
+    let svg = d3.select(this.myRef.current)
+    svg.attr("viewBox", [0, 0, width, height]);
+    svg.append("g")
+      .call(xAxis);
+    svg.append("g")
+      .call(yAxis);
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", line);
+
+
   }
   render() {
     return (
