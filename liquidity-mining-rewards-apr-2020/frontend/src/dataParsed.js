c97c8aae3f1d69b370f5241a0f008081cd12258d
Aidan Musnitzky <musnit@gmail.com>
2021-04-30 22:44:12 -0400

    complete

--- liquidity-mining-rewards-apr-2020/frontend/src/dataParsed.js
+++ liquidity-mining-rewards-apr-2020/frontend/src/dataParsed.js
@@ -3,3 +3,11 @@ import _ from 'lodash';
 
 export const users = _.uniq(_.flatten(data.map(timestamp => Object.keys(timestamp.users))))
 export const raw = data
+export const rewardBucketsTimeSeries = data.map((timestampData, timestamp) => {
+  const rewardBuckets = timestampData.rewardBuckets
+  const totalCurrentRowan = _.sum(rewardBuckets.map(b=>b.rowan))
+  const totalInitialRowan = _.sum(rewardBuckets.map(b=>b.initialRowan))
+  return {
+    timestamp, totalCurrentRowan, totalInitialRowan
+  }
+}).slice(1)
