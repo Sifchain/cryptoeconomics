8549286f6fde5d640dfc61ff90b57170e833d01f
Aidan Musnitzky <musnit@gmail.com>
2021-04-30 21:11:31 -0400

    fix

--- liquidity-mining-rewards-apr-2020/frontend/src/Chart.js
+++ liquidity-mining-rewards-apr-2020/frontend/src/Chart.js
@@ -8,25 +8,22 @@ class Chart extends React.Component {
     this.myRef = React.createRef();
     this.dataset = [100, 200, 300, 400, 500];
   }
-
   componentDidMount() {
-    let size = 500;
-    let svg = d3.select(this.myRef.current)
-    svg.attr("viewBox", [0, 0, width, height]);
-    svg.append("g")
-      .call(xAxis);
-    svg.append("g")
-      .call(yAxis);
-    svg.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("d", line);
-
-
+    // let size = 500;
+    // let svg = d3.select(this.myRef.current)
+    //   .append('svg')
+    //   .attr('width', size)
+    //   .attr('height', size);
+    // let rect_width = 95;
+    // svg.selectAll('rect')
+    //   .data(this.dataset)
+    //   .enter()
+    //   .append('rect')
+    //   .attr('x', (d, i) => 5 + i * (rect_width + 5))
+    //   .attr('y', d => size — d)
+    //   .attr('width', rect_width)
+    //   .attr('height', d => d)
+    //   .attr('fill', 'teal');
   }
   render() {
     return (
