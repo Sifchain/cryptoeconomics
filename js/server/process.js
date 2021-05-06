47cf07a227e81ddb296b338a4f0fbaa6f0212185
Aidan Musnitzky <musnit@gmail.com>
2021-05-05 21:16:12 -0400

    more refactor

--- js/server/process.js
+++ js/server/process.js
@@ -1,6 +1,7 @@
 moment = require("moment")
 _ = require("lodash")
 const { augmentLMData } = require('./augmentLMData')
+const { augmentVSData } = require('./augmentVSData')
 
 const { remapLMAddresses } = require('./util/lm-util');
 const { remapVSAddresses } = require('./util/vs-util');
@@ -15,12 +16,10 @@ const { TIME_INTERVAL,
 const snapshotLM = require("../snapshots/snapshot_lm_latest.json");
 const snapshotVS = require("../snapshots/snapshot_vs_latest.json");
 
-exports.getParsedData = _ => {
+exports.getProcessedLMData = _ => {
   const LMAddresses = snapshotLM.data.snapshots_new[0].snapshot_data
-  const VSValidatorAddresses = snapshotVS.data.snapshots_validators[0].snapshot_data
 
   const LMTimeIntervalEvents = remapLMAddresses(LMAddresses, TIME_INTERVAL)
-  const VSTimeIntervalEvents = remapVSAddresses(VSValidatorAddresses, TIME_INTERVAL)
 
   const LMGlobalStates = [LM_STARTING_GLOBAL_STATE]
   for (let i = 0; i < NUMBER_OF_INTERVALS_TO_RUN; i++) {
@@ -31,17 +30,27 @@ exports.getParsedData = _ => {
     LMGlobalStates.push(newGlobalState)
   }
 
+  // TODO: remove past dispensations
+  // TODO: return unpaid balances
+  return augmentLMData(LMGlobalStates)
+}
+
+exports.getProcessedVSData = _ => {
+  const VSValidatorAddresses = snapshotVS.data.snapshots_validators[0].snapshot_data
+
+  const VSTimeIntervalEvents = remapVSAddresses(VSValidatorAddresses, TIME_INTERVAL)
+
   const VSGlobalStates = [VS_STARTING_GLOBAL_STATE]
   for (let i = 0; i < NUMBER_OF_INTERVALS_TO_RUN; i++) {
     const lastGlobalState = VSGlobalStates[VSGlobalStates.length - 1]
     const timestamp = i * TIME_INTERVAL
     const events = VSTimeIntervalEvents['' + timestamp] || []
-    // const newGlobalState = processVSGlobalState(lastGlobalState, timestamp, events)
-    // VSGlobalStates.push(newGlobalState)
+    const newGlobalState = processVSGlobalState(lastGlobalState, timestamp, events)
+    VSGlobalStates.push(newGlobalState)
   }
 
 
   // TODO: remove past dispensations
   // TODO: return unpaid balances
-  return augmentLMData(LMGlobalStates)
+  return augmentVSData(VSGlobalStates)
 }
