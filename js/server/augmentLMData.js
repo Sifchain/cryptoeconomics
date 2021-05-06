716a95eb61fcc32fddfd0b1f5d48d81f67edccac
Aidan Musnitzky <musnit@gmail.com>
2021-05-05 22:26:10 -0400

    both working

--- js/server/augmentLMData.js
+++ js/server/augmentLMData.js
@@ -5,28 +5,22 @@ const { START_DATETIME } = require("./config");
 exports.augmentLMData = data => {
   const users = _.uniq(_.flatten(data.map(timestamp => Object.keys(timestamp.users))))
 
-  const dataWithRewards = data.map(timestamp => {
+  data.forEach(timestamp => {
     const timestampTotalTickets = _.sum(_.map(timestamp.users, user => {
       return _.sum(user.tickets.map(t => t.amount))
     }))
-    return {
-      ...timestamp,
-      totalTickets: timestampTotalTickets,
-      users: _.mapValues(timestamp.users, user => {
-        const totalTickets = _.sum(user.tickets.map(t => t.amount))
-        return {
-          ...user,
-          claimableReward: user.claimed + _.sum(user.tickets.map(t => t.reward * t.mul)),
-          reservedReward: user.claimed + _.sum(user.tickets.map(t => t.reward)),
-          totalTickets,
-          nextRewardShare: totalTickets / timestampTotalTickets,
-        }
-      })
-    }
+    timestamp.totalTickets = timestampTotalTickets
+    timestamp.users = _.forEach(timestamp.users, user => {
+      const totalTickets = _.sum(user.tickets.map(t => t.amount))
+      user.claimableReward = user.claimed + _.sum(user.tickets.map(t => t.reward * t.mul))
+      user.reservedReward = user.claimed + _.sum(user.tickets.map(t => t.reward))
+      user.totalTickets = totalTickets
+      user.nextRewardShare = totalTickets / timestampTotalTickets
+    })
   })
 
-  const finalTimestamp = dataWithRewards[dataWithRewards.length - 1] || { users: [] };
-  dataWithRewards.forEach((timestamp) => {
+  const finalTimestamp = data[data.length - 1] || { users: [] };
+  data.forEach((timestamp) => {
     _.forEach(timestamp.users, (user, address) => {
       const userAtMaturity = finalTimestamp.users[address] || {}
       user.totalRewardAtMaturity = userAtMaturity.claimableReward
@@ -35,9 +29,9 @@ exports.augmentLMData = data => {
     })
   })
 
-  processedData.forEach((timestamp, timestampIndex) => {
+  data.forEach((timestamp, timestampIndex) => {
     _.forEach(timestamp.users, (user, address) => {
-      const prevTimestamp = processedData[timestampIndex - 1] || { users: [] }
+      const prevTimestamp = data[timestampIndex - 1] || { users: [] }
       const lastUser = prevTimestamp.users[address] || {}
       const lastUserMaturityDate = lastUser.maturityDate
       let maturityDate = lastUserMaturityDate
@@ -54,14 +48,13 @@ exports.augmentLMData = data => {
   })
 
   // fill in old timestamps with maturity date now that we have it
-  const lastTimestamp = processedData[processedData.length - 1] || { users: [] }
-  processedData.forEach((timestamp) => {
+  const lastTimestamp = data[data.length - 1] || { users: [] }
+  data.forEach((timestamp) => {
     _.forEach(timestamp.users, (user, address) => {
       const lastUser = lastTimestamp.users[address] || {}
       user.maturityDate = lastUser.maturityDate
     })
   })
-
 
   const rewardBucketsTimeSeries = data.map((timestampData, timestamp) => {
     const rewardBuckets = timestampData.rewardBuckets
@@ -72,24 +65,24 @@ exports.augmentLMData = data => {
     }
   }).slice(1)
 
-  const stackClaimableRewardData = processedData.map(t => {
-    const blankUsers = users.reduce((accum, user) => {
-      accum[user] = 0
-      return accum
-    }, {});
-    const blankUserRewards = _.mapValues(blankUsers, u => 0)
-    const userRewards = _.mapValues(t.users, u => u.claimableReward)
-    return {
-      timestamp: t.timestamp,
-      ...blankUserRewards,
-      ...userRewards
-    }
-  }).slice(1)
+  // const stackClaimableRewardData = dataWithRewards.map(t => {
+  //   const blankUsers = users.reduce((accum, user) => {
+  //     accum[user] = 0
+  //     return accum
+  //   }, {});
+  //   const blankUserRewards = _.mapValues(blankUsers, u => 0)
+  //   const userRewards = _.mapValues(t.users, u => u.claimableReward)
+  //   return {
+  //     timestamp: t.timestamp,
+  //     ...blankUserRewards,
+  //     ...userRewards
+  //   }
+  // }).slice(1)
 
   return {
     users,
-    processedData,
+    processedData: data,
     rewardBucketsTimeSeries,
-    stackClaimableRewardData,
+    // stackClaimableRewardData,
   }
 }
