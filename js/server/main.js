77cb2b9bba9b6d3d7f359264b7824aea36d6905f
mccallofthewild <mccallalexander96@gmail.com>
2021-05-06 18:11:04 -0600

    completed child process

--- js/server/main.js
+++ js/server/main.js
@@ -1,69 +1,91 @@
 if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config()
+	require('dotenv').config();
 }
-const express = require("express");
-const cors = require('cors')
-const { getUserData, getUserTimeSeriesData } = require('./user');
+const express = require('express');
+const cors = require('cors');
 
 // implements process.js in separate thread
 const { createProcessingHandler } = require('./processing-handler');
-const processingWorkerHandler = createProcessingHandler()
+const processingHandler = createProcessingHandler();
 
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(cors())
-
-
+app.use(cors());
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
 
-
-app.get("/status", (req, res, next) => {
-  res.status(200).send({ status: "OK" });
+app.get('/status', (req, res, next) => {
+	res.status(200).send({ status: 'OK' });
 });
 
 /* 
  TODO: Add timestamp parameter to endpoints 
   e.g. https://sif-cryptoecon-test.herokuapp.com/api?key=userData&address=sif1048c2lagwe84sz0sk4ncy2myrzxykjes40hn62with&timestamp=XYZ
 */
-app.get("/api/lm", async (req, res, next) => {
-  const key = req.query.key;
-  await processingWorkerHandler.waitForReadyState().catch(console.error);
-  const { lmData } = processingWorkerHandler.getCurrentState();
-  let responseJSON = lmData[key];
-  if (key === 'userTimeSeriesData') {
-    const address = req.query.address
-    responseJSON = getUserTimeSeriesData(lmData.processedData, address)
-  }
-  if (key === 'userData') {
-    const address = req.query.address
-    responseJSON = getUserData(lmData.processedData, address)
-  }
-  if (key === 'stack') {
-    rewardData = lmData.stackClaimableRewardData
-    responseJSON = { rewardData }
-  }
-  res.json(responseJSON)
+app.get('/api/lm', async (req, res, next) => {
+	const key = req.query.key;
+	let responseJSON;
+	// switch/case
+	switch (key) {
+		case 'userTimeSeriesData': {
+			const address = req.query.address;
+			responseJSON = await processingHandler.dispatch(
+				'GET_LM_USER_TIME_SERIES_DATA',
+				address
+			);
+			break;
+		}
+		case 'userData': {
+			const address = req.query.address;
+			responseJSON = await processingHandler.dispatch(
+				'GET_LM_USER_DATA',
+				address
+			);
+			break;
+		}
+		case 'stack': {
+			rewardData = await processingHandler.dispatch('GET_LM_STACK_DATA', null);
+			responseJSON = { rewardData };
+			break;
+		}
+		default: {
+			responseJSON = await processingHandler.dispatch('GET_LM_KEY_VALUE', key);
+		}
+	}
+	res.json(responseJSON);
 });
 
-app.get("/api/vs", async (req, res, next) => {
-  const key = req.query.key;
-  await processingWorkerHandler.waitForReadyState().catch(console.error);
-  const { vsData } = processingWorkerHandler.getCurrentState();
-  let responseJSON = vsData[key];
-  if (key === 'userTimeSeriesData') {
-    const address = req.query.address
-    responseJSON = getUserTimeSeriesData(vsData.processedData, address)
-  }
-  if (key === 'userData') {
-    const address = req.query.address
-    responseJSON = getUserData(vsData.processedData, address)
-  }
-  if (key === 'stack') {
-    rewardData = vsData.stackClaimableRewardData
-    responseJSON = { rewardData }
-  }
-  res.json(responseJSON)
+app.get('/api/vs', async (req, res, next) => {
+	const key = req.query.key;
+	let responseJSON;
+	// switch/case
+	switch (key) {
+		case 'userTimeSeriesData': {
+			const address = req.query.address;
+			responseJSON = await processingHandler.dispatch(
+				'GET_VS_USER_TIME_SERIES_DATA',
+				address
+			);
+			break;
+		}
+		case 'userData': {
+			const address = req.query.address;
+			responseJSON = await processingHandler.dispatch(
+				'GET_VS_USER_DATA',
+				address
+			);
+			break;
+		}
+		case 'stack': {
+			rewardData = await processingHandler.dispatch('GET_VS_STACK_DATA', null);
+			responseJSON = { rewardData };
+			break;
+		}
+		default: {
+			responseJSON = await processingHandler.dispatch('GET_VS_KEY_VALUE', key);
+		}
+	}
+	res.json(responseJSON);
 });
--- js/server/processing-handler.js
+++ js/server/processing-handler.js
@@ -1,64 +1,69 @@
-
-const { fork } = require("child_process")
-
+const { fork } = require('child_process');
+const bfj = require('bfj');
 function createProcessingHandler() {
-  let hasLoaded = false;
-  let didExit = false;
-  let didError = false;
-  let currentState = {
-    lmData: null,
-    vsData: null,
-    // denotes the time at which client is suggested to reload data
-    expiresAt: null
-  }
+	let hasLoaded = false;
+	let didExit = false;
+	let didError = false;
+	let currentState = {
+		lmData: null,
+		vsData: null,
+		// denotes the time at which client is suggested to reload data
+		expiresAt: null,
+	};
 
-  const childProcess = fork(`${__dirname}/process.childprocess.js`) //the first argument to fork() is the name of the js file to be run by the child process
-  // childProcess.send({ number: parseInt(req.query.number) }) //send method is used to send message to child process through IPC
-  childProcess.on("message", async outputFilePath => {
-    hasLoaded = true;
-    const dataString = await new Promise(r => require('fs').readFile(outputFilePath.toString(), undefined, (err, data) => r(data)))
-    const {
-      lm,
-      vs,
-      expiresAt
-    } = JSON.parse(dataString)
-    currentState = {
-      ...currentState,
-      lmData: lm,
-      vsData: vs,
-      expiresAt
-    }
-  })
-  childProcess.on("error", e => {
-    didError = true;
-    console.error(e);
-  });
-  childProcess.on("exit", code  => {
-    didExit = true;
-    if (code  !==  0) {
-      console.error(new Error(`Child processs stopped with exit code ${code}`));
-    }
-  });
-  return {
-    getCurrentState () {
-      if (!hasLoaded) {
-        throw new Error('Await #waitForReadyState before accessing state')
-      }
-      return currentState;
-    },
-    async waitForReadyState () {
-      return new Promise(async (r, rj) => {
-        while (true) {
-          if (didError) rj('errored');
-          if (didExit) rj('exited');
-          if (hasLoaded) r(true);
-          await new Promise(r => setTimeout(r, 100))
-        }
-      })
-    }
-  }
-};
+	const childProcess = fork(`${__dirname}/process.childprocess.js`); //the first argument to fork() is the name of the js file to be run by the child process
+
+	childProcess.on('error', (e) => {
+		didError = true;
+		console.error(e);
+	});
+	childProcess.on('exit', (code) => {
+		didExit = true;
+		if (code !== 0) {
+			console.error(new Error(`Child processs stopped with exit code ${code}`));
+		}
+	});
+	async function dispatch(method, payload) {
+		const invokationId = Math.random().toString();
+		childProcess.send({
+			action: 'invoke',
+			payload: {
+				fn: method,
+				args: [payload],
+				id: invokationId,
+			},
+		});
+		return new Promise((r, rj) => {
+			const handler = async (msg) => {
+				if (typeof msg != 'object' || msg.action !== 'return' || !msg.payload)
+					return;
+				if (msg.payload.id !== invokationId) return;
+				r(msg.payload.out);
+				childProcess.off('message', handler);
+			};
+			childProcess.on('message', handler);
+		});
+	}
+	async function waitForReadyState() {
+		return new Promise(async (r, rj) => {
+			while (true) {
+				let isReady = await dispatch('CHECK_IF_PARSED_DATA_READY');
+				if (didError) rj('errored');
+				if (didExit) rj('exited');
+				if (isReady) return r(true);
+				await new Promise((r) => setTimeout(r, 100));
+			}
+		});
+	}
+	return {
+		// Remotely invokes child process actions in `./process.childprocess.js` `BackgroundProcessor#actions`
+		async dispatch(method, payload) {
+			await waitForReadyState();
+			return dispatch(method, payload);
+		},
+	};
+}
 
 module.exports = {
-  createProcessingHandler
-}
+	createProcessingHandler,
+};
