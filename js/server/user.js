4c735eda5f7c7041434e79d2c5e78aa5a565408e
Andrew Joslin <andrewtjoslin@gmail.com>
2021-05-07 13:39:38 -0400

    Lint merge from master

--- js/server/user.js
+++ js/server/user.js
@@ -1,13 +1,21 @@
 exports.getUserTimeSeriesData = (all, address) => {
-  return all.map((timestampData) => {
-    const userData = timestampData.users[address] || { tickets: [], reservedReward: 0, claimableReward: 0 };
-    const userClaimableReward = userData.claimableReward
-    const userReservedReward = userData.reservedReward
-    return {
-      timestamp: timestampData.timestamp, userClaimableReward, userReservedReward
-    }
-  }).slice(1)
-}
+  return all
+    .map(timestampData => {
+      const userData = timestampData.users[address] || {
+        tickets: [],
+        reservedReward: 0,
+        claimableReward: 0
+      };
+      const userClaimableReward = userData.claimableReward;
+      const userReservedReward = userData.reservedReward;
+      return {
+        timestamp: timestampData.timestamp,
+        userClaimableReward,
+        userReservedReward
+      };
+    })
+    .slice(1);
+};
 
 exports.getUserData = (all, address, timeIndex) => {
   const data = all.map(timestampGlobalState => {
@@ -15,8 +23,10 @@ exports.getUserData = (all, address, timeIndex) => {
       ...timestampGlobalState,
       users: undefined,
       user: timestampGlobalState.users[address]
-    }
-  })
-  if (!timeIndex) { return data }
-  return data[timeIndex]
-}
+    };
+  });
+  if (!timeIndex) {
+    return data;
+  }
+  return data[timeIndex];
+};
--- js/src/api.js
+++ js/src/api.js
@@ -1,32 +1,36 @@
 const serverURL = (() => {
   switch (process.env.REACT_APP_DEPLOYMENT_TAG) {
-    case "production":
-      return `https://api-cryptoeconomics.sifchain.finance/api`
-    case "devnet":
-      return `https://api-cryptoeconomics-devnet.sifchain.finance/api`;
-    case "testnet":
-      return `https://api-cryptoeconomics-testnet.sifchain.finance/api`;
+    case 'production':
+      return 'https://api-cryptoeconomics.sifchain.finance/api';
+    case 'devnet':
+      return 'https://api-cryptoeconomics-devnet.sifchain.finance/api';
+    case 'testnet':
+      return 'https://api-cryptoeconomics-testnet.sifchain.finance/api';
     default:
-      return `http://localhost:3000/api`;
+      return 'http://localhost:3000/api';
   }
-})()
+})();
 
 export const fetchUsers = type => {
-  return fetch(`${serverURL}/${type}?key=users`)
-    .then(response => response.json())
-}
+  return window
+    .fetch(`${serverURL}/${type}?key=users`)
+    .then(response => response.json());
+};
 
 export const fetchUserData = (address, type) => {
-  return fetch(`${serverURL}/${type}?key=userData&address=${address}`)
-    .then(response => response.json())
-}
+  return window
+    .fetch(`${serverURL}/${type}?key=userData&address=${address}`)
+    .then(response => response.json());
+};
 
 export const fetchUserTimeSeriesData = (address, type) => {
-  return fetch(`${serverURL}/${type}?key=userTimeSeriesData&address=${address}`)
-    .then(response => response.json())
-}
+  return window
+    .fetch(`${serverURL}/${type}?key=userTimeSeriesData&address=${address}`)
+    .then(response => response.json());
+};
 
 export const fetchStack = type => {
-  return fetch(`${serverURL}/${type}?key=stack`)
-    .then(response => response.json())
-}
+  return window
+    .fetch(`${serverURL}/${type}?key=stack`)
+    .then(response => response.json());
+};
