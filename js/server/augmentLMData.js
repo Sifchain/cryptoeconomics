8d1b5ba5c701086d4d74d028bd0e4d87cda34ee5
mccallofthewild <mccallalexander96@gmail.com>
2021-05-07 19:32:42 -0600

    removed comments

--- js/server/augmentLMData.js
+++ js/server/augmentLMData.js
@@ -1,37 +1,37 @@
-const _ = require("lodash");
-const moment = require("moment");
-const { START_DATETIME } = require("./config");
+const _ = require('lodash');
+const moment = require('moment');
+const { START_DATETIME } = require('./config');
 
-exports.augmentLMData = (data) => {
+exports.augmentLMData = data => {
   const users = _.uniq(
-    _.flatten(data.map((timestamp) => Object.keys(timestamp.users)))
+    _.flatten(data.map(timestamp => Object.keys(timestamp.users)))
   );
 
-  data.forEach((timestamp) => {
+  data.forEach(timestamp => {
     const timestampTotalTickets = _.sum(
-      _.map(timestamp.users, (user) => {
-        return _.sum(user.tickets.map((t) => t.amount));
+      _.map(timestamp.users, user => {
+        return _.sum(user.tickets.map(t => t.amount));
       })
     );
     timestamp.totalTickets = timestampTotalTickets;
-    timestamp.users = _.forEach(timestamp.users, (user) => {
-      const totalTickets = _.sum(user.tickets.map((t) => t.amount));
+    timestamp.users = _.forEach(timestamp.users, user => {
+      const totalTickets = _.sum(user.tickets.map(t => t.amount));
       user.claimableReward =
-        user.claimed + _.sum(user.tickets.map((t) => t.reward * t.mul));
+        user.claimed + _.sum(user.tickets.map(t => t.reward * t.mul));
       user.reservedReward =
-        user.claimed + _.sum(user.tickets.map((t) => t.reward));
+        user.claimed + _.sum(user.tickets.map(t => t.reward));
       user.totalTickets = totalTickets;
       user.nextRewardShare = totalTickets / timestampTotalTickets;
     });
   });
 
   const finalTimestamp = data[data.length - 1] || { users: [] };
-  data.forEach((timestamp) => {
+  data.forEach(timestamp => {
     _.forEach(timestamp.users, (user, address) => {
       const userAtMaturity = finalTimestamp.users[address] || {};
       user.totalRewardAtMaturity = userAtMaturity.claimableReward;
       user.ticketAmountAtMaturity = _.sum(
-        finalTimestamp.users[address].tickets.map((ticket) => ticket.amount)
+        finalTimestamp.users[address].tickets.map(ticket => ticket.amount)
       );
       user.yieldAtMaturity =
         user.totalRewardAtMaturity / user.ticketAmountAtMaturity;
@@ -55,8 +55,8 @@ exports.augmentLMData = (data) => {
       ) {
         maturityDate = moment
           .utc(START_DATETIME)
-          .add(timestamp.timestamp, "m")
-          .format("MMMM Do YYYY, h:mm:ss a");
+          .add(timestamp.timestamp, 'm')
+          .format('MMMM Do YYYY, h:mm:ss a');
       }
       if (
         maturityDateISO === undefined && // maturity date not yet reached
@@ -65,7 +65,7 @@ exports.augmentLMData = (data) => {
       ) {
         maturityDateISO = user.maturityDateISO = moment
           .utc(START_DATETIME)
-          .add(timestamp.timestamp, "m")
+          .add(timestamp.timestamp, 'm')
           .toISOString();
       }
       user.maturityDate = maturityDate;
@@ -75,7 +75,7 @@ exports.augmentLMData = (data) => {
 
   // fill in old timestamps with maturity date now that we have it
   const lastTimestamp = data[data.length - 1] || { users: [] };
-  data.forEach((timestamp) => {
+  data.forEach(timestamp => {
     _.forEach(timestamp.users, (user, address) => {
       const lastUser = lastTimestamp.users[address] || {};
       user.maturityDate = lastUser.maturityDate;
@@ -86,12 +86,12 @@ exports.augmentLMData = (data) => {
   const rewardBucketsTimeSeries = data
     .map((timestampData, timestamp) => {
       const rewardBuckets = timestampData.rewardBuckets;
-      const totalCurrentRowan = _.sum(rewardBuckets.map((b) => b.rowan));
-      const totalInitialRowan = _.sum(rewardBuckets.map((b) => b.initialRowan));
+      const totalCurrentRowan = _.sum(rewardBuckets.map(b => b.rowan));
+      const totalInitialRowan = _.sum(rewardBuckets.map(b => b.initialRowan));
       return {
         timestamp,
         totalCurrentRowan,
-        totalInitialRowan,
+        totalInitialRowan
       };
     })
     .slice(1);
@@ -99,12 +99,12 @@ exports.augmentLMData = (data) => {
   const stackClaimableRewardData = [];
   const finalTimestampUsers = _.map(finalTimestamp.users, (u, address) => ({
     ...u,
-    address,
+    address
   }));
   const top50Users = _.orderBy(
     finalTimestampUsers,
-    ["totalRewardAtMaturity"],
-    ["desc"]
+    ['totalRewardAtMaturity'],
+    ['desc']
   ).slice(0, 50);
   const blankUserRewards = top50Users.reduce((accum, user) => {
     accum[user.address] = 0;
@@ -122,7 +122,7 @@ exports.augmentLMData = (data) => {
     stackClaimableRewardData.push({
       timestamp: timestamp.timestamp,
       ...blankUserRewards,
-      ...userRewards,
+      ...userRewards
     });
   }
 
@@ -130,6 +130,6 @@ exports.augmentLMData = (data) => {
     users,
     processedData: data,
     rewardBucketsTimeSeries,
-    stackClaimableRewardData,
+    stackClaimableRewardData
   };
 };
--- js/server/main.js
+++ js/server/main.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const { getTimeIndex } = require("./util/getTimeIndex");
+const express = require('express');
+const cors = require('cors');
+const { getTimeIndex } = require('./util/getTimeIndex');
 
 // implements process.js in separate thread
-const { createMultiprocessActionDispatcher } = require("./processing-handler");
+const { createMultiprocessActionDispatcher } = require('./processing-handler');
 
 // interfaces with `./process.childprocess.js`
 const processingHandler = createMultiprocessActionDispatcher();
@@ -16,78 +16,78 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get("/status", (req, res, next) => {
-  res.status(200).send({ status: "OK" });
+app.get('/status', (req, res, next) => {
+  res.status(200).send({ status: 'OK' });
 });
 
-app.get("/api/lm", async (req, res, next) => {
+app.get('/api/lm', async (req, res, next) => {
   const key = req.query.key;
   let responseJSON;
   const activeProcess = processingHandler.getActiveProcess();
   await activeProcess.waitForReadyState();
   switch (key) {
-    case "userTimeSeriesData": {
+    case 'userTimeSeriesData': {
       const address = req.query.address;
       responseJSON = await activeProcess.dispatch(
-        "GET_LM_USER_TIME_SERIES_DATA",
+        'GET_LM_USER_TIME_SERIES_DATA',
         address
       );
       break;
     }
-    case "userData": {
+    case 'userData': {
       const address = req.query.address;
       const timeIndex = getTimeIndex(req.query.timestamp);
-      responseJSON = await activeProcess.dispatch("GET_LM_USER_DATA", {
+      responseJSON = await activeProcess.dispatch('GET_LM_USER_DATA', {
         address,
-        timeIndex,
+        timeIndex
       });
       break;
     }
-    case "stack": {
-      let rewardData = await activeProcess.dispatch("GET_LM_STACK_DATA", null);
+    case 'stack': {
+      let rewardData = await activeProcess.dispatch('GET_LM_STACK_DATA', null);
       responseJSON = { rewardData };
       break;
     }
-    case "maturityAPY": {
+    case 'maturityAPY': {
       const address = req.query.address;
-      responseJSON = await activeProcess.dispatch("GET_LM_USER_MATURITY_APY", {
-        address,
+      responseJSON = await activeProcess.dispatch('GET_LM_USER_MATURITY_APY', {
+        address
       });
       break;
     }
     default: {
-      responseJSON = await activeProcess.dispatch("GET_LM_KEY_VALUE", key);
+      responseJSON = await activeProcess.dispatch('GET_LM_KEY_VALUE', key);
     }
   }
   res.json(responseJSON);
 });
 
-app.get("/api/vs", async (req, res, next) => {
+app.get('/api/vs', async (req, res, next) => {
   const key = req.query.key;
   let responseJSON;
   const activeProcess = processingHandler.getActiveProcess();
   await activeProcess.waitForReadyState();
   switch (key) {
-    case "userTimeSeriesData": {
+    case 'userTimeSeriesData': {
       const address = req.query.address;
       responseJSON = await activeProcess.dispatch(
-        "GET_VS_USER_TIME_SERIES_DATA",
+        'GET_VS_USER_TIME_SERIES_DATA',
         address
       );
       break;
     }
-    case "userData": {
+    case 'userData': {
       const address = req.query.address;
-      responseJSON = await activeProcess.dispatch("GET_VS_USER_DATA", address);
+      responseJSON = await activeProcess.dispatch('GET_VS_USER_DATA', address);
       break;
     }
-    case "stack": {
-      let rewardData = await activeProcess.dispatch("GET_VS_STACK_DATA", null);
+    case 'stack': {
+      let rewardData = await activeProcess.dispatch('GET_VS_STACK_DATA', null);
       responseJSON = { rewardData };
       break;
     }
     default: {
-      responseJSON = await activeProcess.dispatch("GET_VS_KEY_VALUE", key);
+      responseJSON = await activeProcess.dispatch('GET_VS_KEY_VALUE', key);
     }
   }
   res.json(responseJSON);
