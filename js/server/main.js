22deaac07ab5d32f83380aba0df1f92a5397edbb
mccallofthewild <mccallalexander96@gmail.com>
2021-05-11 15:08:45 -0600

    removed everything except /status endpoint

--- js/server/main.js
+++ js/server/main.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { getTimeIndex } = require('./util/getTimeIndex');
+// const { getTimeIndex } = require("./util/getTimeIndex");
 
 // implements process.js in separate thread
-const { createMultiprocessActionDispatcher } = require('./processing-handler');
+// const { createMultiprocessActionDispatcher } = require('./processing-handler');
 
 // interfaces with `./process.childprocess.js`
-const processingHandler = createMultiprocessActionDispatcher();
+// const processingHandler = createMultiprocessActionDispatcher();
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -20,75 +20,75 @@ app.get('/status', (req, res, next) => {
   res.status(200).send({ status: 'OK' });
 });
 
-app.get('/api/lm', async (req, res, next) => {
-  const key = req.query.key;
-  let responseJSON;
-  const activeProcess = processingHandler.getActiveProcess();
-  await activeProcess.waitForReadyState();
-  switch (key) {
-    case 'userTimeSeriesData': {
-      const address = req.query.address;
-      responseJSON = await activeProcess.dispatch(
-        'GET_LM_USER_TIME_SERIES_DATA',
-        address
-      );
-      break;
-    }
-    case 'userData': {
-      const address = req.query.address;
-      const timeIndex = getTimeIndex(req.query.timestamp);
-      responseJSON = await activeProcess.dispatch('GET_LM_USER_DATA', {
-        address,
-        timeIndex
-      });
-      break;
-    }
-    case 'stack': {
-      let rewardData = await activeProcess.dispatch('GET_LM_STACK_DATA', null);
-      responseJSON = { rewardData };
-      break;
-    }
-    default: {
-      responseJSON = await activeProcess.dispatch('GET_LM_KEY_VALUE', key);
-    }
-  }
-  res.json(responseJSON);
-});
+// app.get('/api/lm', async (req, res, next) => {
+//   const key = req.query.key;
+//   let responseJSON;
+//   const activeProcess = processingHandler.getActiveProcess();
+//   await activeProcess.waitForReadyState();
+//   switch (key) {
+//     case 'userTimeSeriesData': {
+//       const address = req.query.address;
+//       responseJSON = await activeProcess.dispatch(
+//         'GET_LM_USER_TIME_SERIES_DATA',
+//         address
+//       );
+//       break;
+//     }
+//     case 'userData': {
+//       const address = req.query.address;
+//       const timeIndex = getTimeIndex(req.query.timestamp);
+//       responseJSON = await activeProcess.dispatch('GET_LM_USER_DATA', {
+//         address,
+//         timeIndex
+//       });
+//       break;
+//     }
+//     case 'stack': {
+//       let rewardData = await activeProcess.dispatch('GET_LM_STACK_DATA', null);
+//       responseJSON = { rewardData };
+//       break;
+//     }
+//     default: {
+//       responseJSON = await activeProcess.dispatch('GET_LM_KEY_VALUE', key);
+//     }
+//   }
+//   res.json(responseJSON);
+// });
 
-app.get('/api/vs', async (req, res, next) => {
-  const key = req.query.key;
-  let responseJSON;
-  const activeProcess = processingHandler.getActiveProcess();
-  await activeProcess.waitForReadyState();
-  switch (key) {
-    case 'unclaimedDelegatedRewards': {
-      break;
-    }
-    case 'userTimeSeriesData': {
-      const address = req.query.address;
-      responseJSON = await activeProcess.dispatch(
-        'GET_VS_USER_TIME_SERIES_DATA',
-        address
-      );
-      break;
-    }
-    case 'userData': {
-      const address = req.query.address;
-      const timeIndex = getTimeIndex(req.query.timestamp);
-      responseJSON = await activeProcess.dispatch('GET_VS_USER_DATA', {
-        address,
-        timeIndex
-      });
-      break;
-    }
-    case 'stack': {
-      let rewardData = await activeProcess.dispatch('GET_VS_STACK_DATA', null);
-      responseJSON = { rewardData };
-      break;
-    }
-    default: {
-      responseJSON = await activeProcess.dispatch('GET_VS_KEY_VALUE', key);
-    }
-  }
-  res.json(responseJSON);
-});
+// app.get('/api/vs', async (req, res, next) => {
+//   const key = req.query.key;
+//   let responseJSON;
+//   const activeProcess = processingHandler.getActiveProcess();
+//   await activeProcess.waitForReadyState();
+//   switch (key) {
+//     case 'unclaimedDelegatedRewards': {
+//       break;
+//     }
+//     case 'userTimeSeriesData': {
+//       const address = req.query.address;
+//       responseJSON = await activeProcess.dispatch(
+//         'GET_VS_USER_TIME_SERIES_DATA',
+//         address
+//       );
+//       break;
+//     }
+//     case 'userData': {
+//       const address = req.query.address;
+//       const timeIndex = getTimeIndex(req.query.timestamp);
+//       responseJSON = await activeProcess.dispatch('GET_VS_USER_DATA', {
+//         address,
+//         timeIndex
+//       });
+//       break;
+//     }
+//     case 'stack': {
+//       let rewardData = await activeProcess.dispatch('GET_VS_STACK_DATA', null);
+//       responseJSON = { rewardData };
+//       break;
+//     }
+//     default: {
+//       responseJSON = await activeProcess.dispatch('GET_VS_KEY_VALUE', key);
+//     }
+//   }
+//   res.json(responseJSON);
+// });
