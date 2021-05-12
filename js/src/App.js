87d5992e86614de712bc4efe0ca0ddc4a8903081
mccallofthewild <mccallalexander96@gmail.com>
2021-05-12 01:50:00 -0600

    fixed fromEntries bug and centered items on page

--- js/src/App.js
+++ js/src/App.js
@@ -1,22 +1,22 @@
-import { START_DATETIME } from './config';
-import './App.css';
-import React from 'react';
-import { fetchUsers, fetchUserData, fetchUserTimeSeriesData } from './api';
-import JSONPretty from 'react-json-pretty';
-import 'react-json-pretty/themes/monikai.css';
-import moment from 'moment';
-import Chart from './Chart';
-import StackAll from './StackAll';
+import { START_DATETIME } from "./config";
+import "./App.css";
+import React from "react";
+import { fetchUsers, fetchUserData, fetchUserTimeSeriesData } from "./api";
+import JSONPretty from "react-json-pretty";
+import "react-json-pretty/themes/monikai.css";
+import moment from "moment";
+import Chart from "./Chart";
+import StackAll from "./StackAll";
 
 const userFieldsToHide = [
-  'reservedReward',
-  'nextRewardShare',
-  'ticketAmountAtMaturity',
-  'yieldAtMaturity',
-  'nextReward',
-  'nextRewardProjectedFutureReward',
-  'yearsToMaturity',
-  'currentAPYOnTickets'
+  "reservedReward",
+  "nextRewardShare",
+  "ticketAmountAtMaturity",
+  "yieldAtMaturity",
+  "nextReward",
+  "nextRewardProjectedFutureReward",
+  "yearsToMaturity",
+  "currentAPYOnTickets",
 ];
 
 // const now = moment.utc(Date.parse(new Date()));
@@ -25,15 +25,15 @@ const userFieldsToHide = [
 // }
 
 class App extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
-    const [address, type] = window.location.hash.substr(1).split('&type=');
+    const [address, type] = window.location.hash.substr(1).split("&type=");
     this.state = {
       timestamp: 0,
-      date: moment.utc(START_DATETIME).format('MMMM Do YYYY, h:mm:ss a'),
-      address: address || 'none',
-      type: type || 'lm'
+      date: moment.utc(START_DATETIME).format("MMMM Do YYYY, h:mm:ss a"),
+      address: address || "none",
+      type: type || "lm",
     };
 
     this.updateAddressEvent = this.updateAddressEvent.bind(this);
@@ -43,136 +43,139 @@ class App extends React.Component {
     this.updateAddress(this.state.address);
   }
 
-  initDateTime () {
+  initDateTime() {
     const now = moment.utc(Date.parse(new Date()));
     this.setState({
-      date: moment.utc(now).format('MMMM Do YYYY, h:mm:ss a'),
+      date: moment.utc(now).format("MMMM Do YYYY, h:mm:ss a"),
       timestamp: Math.floor(
         moment.duration(now.diff(START_DATETIME)).asMinutes() / 200
-      )
+      ),
     });
   }
 
-  componentDidMount () {
-    fetchUsers('lm').then(usersLM => this.setState({ usersLM }));
-    fetchUsers('vs').then(usersVS => this.setState({ usersVS }));
+  componentDidMount() {
+    fetchUsers("lm").then((usersLM) => this.setState({ usersLM }));
+    fetchUsers("vs").then((usersVS) => this.setState({ usersVS }));
     this.initDateTime();
   }
 
-  updateAddressEvent (event) {
+  updateAddressEvent(event) {
     const address = event.target.value;
     this.updateAddress(address);
   }
 
-  updateAddress (address) {
+  updateAddress(address) {
     window.history.pushState(
       undefined,
-      '',
+      "",
       `#${address}&type=${this.state.type}`
     );
-    if (address !== 'all' && address !== 'none') {
-      fetchUserTimeSeriesData(address, this.state.type).then(
-        userTimeSeriesData => this.setState({ userTimeSeriesData })
-      );
-      fetchUserData(address, this.state.type).then(userData =>
+    if (address !== "all" && address !== "none") {
+      fetchUserTimeSeriesData(
+        address,
+        this.state.type
+      ).then((userTimeSeriesData) => this.setState({ userTimeSeriesData }));
+      fetchUserData(address, this.state.type).then((userData) =>
         this.setState({ userData })
       );
     }
     this.setState({
       address,
       userData: undefined,
-      userTimeSeriesData: undefined
+      userTimeSeriesData: undefined,
     });
   }
 
-  updateTimestamp (event) {
+  updateTimestamp(event) {
     const timestamp = parseInt(event.target.value);
     const minutes = timestamp * 200;
     const date = moment
       .utc(START_DATETIME)
-      .add(minutes, 'm')
-      .format('MMMM Do YYYY, h:mm:ss a');
+      .add(minutes, "m")
+      .format("MMMM Do YYYY, h:mm:ss a");
     this.setState({
       date,
-      timestamp
+      timestamp,
     });
   }
 
-  updateType (event) {
+  updateType(event) {
     const type = event.target.value;
-    window.history.pushState(undefined, '', `#none&type=${this.state.type}`);
+    window.history.pushState(undefined, "", `#none&type=${this.state.type}`);
     this.setState({
       type,
-      address: 'none'
+      address: "none",
     });
   }
 
-  render () {
+  render() {
     if (!this.state.usersLM || !this.state.usersVS) {
       return <div>Loading...</div>;
     }
     const users =
-      this.state.type === 'lm' ? this.state.usersLM : this.state.usersVS;
+      this.state.type === "lm" ? this.state.usersLM : this.state.usersVS;
 
-    let userTimestampJSON = 'Loading...';
+    let userTimestampJSON = "Loading...";
     if (this.state.userData) {
       const data = this.state.userData[this.state.timestamp + 1];
       userTimestampJSON = {
         ...data,
-        user: Object.fromEntries(
-          Object.entries(data.user).filter(([key, val]) => {
-            return !userFieldsToHide.includes(key);
-          })
-        )
+        user: !data.user
+          ? null
+          : Object.fromEntries(
+              Object.entries(data.user).filter(([key, val]) => {
+                return !userFieldsToHide.includes(key);
+              })
+            ),
       };
       console.log(data, userTimestampJSON);
     }
 
     const timeSeriesData = this.state.userTimeSeriesData;
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <div className='logo-container'>
-            <img src='Sifchain-logo-gold.svg' />
+      <div className="App">
+        <header className="App-header">
+          <div className="logo-container">
+            <img src="Sifchain-logo-gold.svg" />
           </div>
-          <div className='select-container'>
-            <div className='radios'>
+          <div className="select-container">
+            <div className="radios">
               <label>
                 <input
-                  type='radio'
-                  value='lm'
-                  onChange={e => this.updateType(e)}
-                  checked={this.state.type === 'lm'}
+                  type="radio"
+                  value="lm"
+                  onChange={(e) => this.updateType(e)}
+                  checked={this.state.type === "lm"}
                 />
                 <span>Liquidity Pooling Rewards</span>
               </label>
               <label>
                 <input
-                  type='radio'
-                  value='vs'
-                  onChange={e => this.updateType(e)}
-                  checked={this.state.type === 'vs'}
+                  type="radio"
+                  value="vs"
+                  onChange={(e) => this.updateType(e)}
+                  checked={this.state.type === "vs"}
                 />
                 <span>Validator Staking/Delegating Rewards</span>
               </label>
             </div>
 
-            <div className='address-container'>
-              Address to Show:{' '}
+            <div className="address-container">
+              Address to Show:{" "}
               <select
                 value={this.state.address}
-                onChange={e => this.updateAddressEvent(e)}
-                className='dropdown-container'
+                onChange={(e) => this.updateAddressEvent(e)}
+                className="dropdown-container"
               >
-                <option key='none' value='none'>
+                <option key="none" value="none">
                   Select An Address
                 </option>
                 {
-                  <option key='all' value='all'>
+                  <option key="all" value="all">
                     Top 50
                   </option>
                 }
-                {users.sort().map(user => (
+                {users.sort().map((user) => (
                   <option key={user} value={user}>
                     {user}
                   </option>
@@ -182,35 +185,39 @@ class App extends React.Component {
           </div>
         </header>
 
-        <div className='content'>
-          {this.state.address === 'all' && <StackAll type={this.state.type} />}
-          {this.state.address !== 'all' &&
-            this.state.address !== 'none' &&
+        <div className="content">
+          {this.state.address === "all" && <StackAll type={this.state.type} />}
+          {this.state.address !== "all" &&
+            this.state.address !== "none" &&
             !timeSeriesData && <div>Loading...</div>}
-          {this.state.address !== 'all' &&
-            this.state.address !== 'none' &&
+          {this.state.address !== "all" &&
+            this.state.address !== "none" &&
             timeSeriesData && <Chart data={timeSeriesData} />}
-          {this.state.address !== 'all' && this.state.address !== 'none' && (
-            <div className='timestamp-slider-description'>
-              Timestamp: {this.state.date}{' '}
+          {this.state.address !== "all" && this.state.address !== "none" && (
+            <div className="timestamp-slider-description">
+              Timestamp: {this.state.date}{" "}
             </div>
           )}
-          {this.state.address !== 'all' &&
-            this.state.address !== 'none' &&
+          {this.state.address !== "all" &&
+            this.state.address !== "none" &&
             timeSeriesData && (
               <input
-                id='timestamp'
-                className='timestamp-slider'
-                type='range'
-                min='0'
+                id="timestamp"
+                className="timestamp-slider"
+                type="range"
+                min="0"
                 max={timeSeriesData.length - 1}
                 value={this.state.timestamp}
-                onChange={e => this.updateTimestamp(e)}
-                step='1'
+                onChange={(e) => this.updateTimestamp(e)}
+                step="1"
               />
             )}
-          {this.state.address !== 'all' && this.state.address !== 'none' && (
-            <JSONPretty id='json-pretty' data={userTimestampJSON} />
+          {this.state.address !== "all" && this.state.address !== "none" && (
+            <JSONPretty
+              className="json-metadata"
+              id="json-pretty"
+              data={userTimestampJSON}
+            />
           )}
         </div>
       </div>
