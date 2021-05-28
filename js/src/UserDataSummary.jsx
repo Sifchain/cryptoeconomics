a794fec55caad50cbd007fdce8e00c264e17901e
mccallofthewild <mccallalexander96@gmail.com>
2021-05-27 22:53:15 -0600

    add user data summary component

---
+++ js/src/UserDataSummary.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+const numFormatter = new Intl.NumberFormat();
+
+const styleData = (data) => ({
+  __styled: true,
+  data,
+});
+const say = (template, ...substitutions) => {
+  // if (!substitutions.every((s) => !!s)) {
+  //   return null;
+  // }
+  return template.map((str) => {
+    let item = substitutions.shift();
+    const isStyled = item && item.__styled;
+    item = isStyled ? item.data : item;
+    const formattedData =
+      typeof item === 'number'
+        ? numFormatter.format(+item.toFixed(2))
+        : item || '';
+    const style =
+      isStyled || typeof item === 'number'
+        ? { color: 'var(--accent-blue)', fontWeight: 700 }
+        : {};
+    return (
+      <span key={item}>
+        {str}
+        <span style={style}>{formattedData}</span>
+      </span>
+    );
+  });
+};
+export const UserDataSummary = ({ user, type = 'vs' }) => {
+  if (!user) return null;
+  if (
+    type !== 'vs' ||
+    !(
+      !process.env.REACT_APP_DEPLOYMENT_TAG ||
+      process.env.REACT_APP_DEPLOYMENT_TAG === 'devnet'
+    )
+  ) {
+    return null;
+  }
+  const validatorText =
+    user.claimableCommissions +
+      user.currentTotalCommissionsOnClaimableDelegatorRewards !==
+    0 ? (
+      <div>
+        {say`As a validator, you have ${styleData(
+          user.claimableCommissions
+        )} rowan in commissions ready to claim. 
+  ${say`And ${styleData(
+    user.currentTotalCommissionsOnClaimableDelegatorRewards
+  )} rowan in commissions will also be claimable when they reach maturity or when their respective delegators claim their rewards.`}`}
+      </div>
+    ) : null;
+  const stakerText = say`Thanks to your total ${
+    type === 'vs' ? 'stake' : 'pool contribution'
+  } of ${user.totalDepositedAmount} rowan, you can claim ${styleData(
+    user.totalClaimableRewardsOnDepositedAssets
+  )} rowan in rewards today. `;
+  const stakerText2 = say`${
+    user.reservedReward > user.totalClaimableRewardsOnDepositedAssets
+      ? say`But if you wait until ${styleData(
+          user.maturityDate
+        )}, you could claim up to ${
+          user.totalRewardsOnDepositedAssetsAtMaturity
+        } rowan. ${say`(a projected APY of ${styleData(
+          user.nextRewardProjectedAPYOnTickets * 100
+        )}%)`}`
+      : null
+  }`;
+  return (
+    <div style={{ width: '100%', padding: '0% 0%' }}>
+      <div className="user-data-summary-container">
+        <div style={{}}>
+          {stakerText}
+          {stakerText2}
+        </div>
+        {validatorText}
+      </div>
+    </div>
+  );
+};
