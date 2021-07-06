fb0a3a8f7a78e70594021bcfc65d3480df77c7cd
McCall Alexander <mccallalexander96@gmail.com>
2021-07-06 12:55:59 -0600

    updated frontend

--- js/src/api.js
+++ js/src/api.js
@@ -13,28 +13,28 @@ const serverURL = (() => {
   }
 })();
 
-const getSnapshotNetworkHeaders = (network) => ({
-  'snapshot-source': network || networks.MAINNET,
+const getSnapshotNetworkHeaders = network => ({
+  'snapshot-source': network || networks.MAINNET
 });
 
-function handleFailedRequest() {
+function handleFailedRequest () {
   setTimeout(() => {
     window.location.reload();
   }, 3000);
 }
-const getUrl = (network) => {
+const getUrl = network => {
   let url = serverURL;
   if (networks.MAINNET_SNAPSHOTS_V2_BETA === network) {
-    url = 'http://ec2-3-138-153-106.us-east-2.compute.amazonaws.com:3000/api';
+    url = 'http://ec2-3-138-153-106.us-east-2.compute.amazonaws.com/api';
   }
   return url;
 };
 export const fetchUsers = (type, network) => {
   return window
     .fetch(`${getUrl(network)}/${type}?key=users`, {
-      headers: getSnapshotNetworkHeaders(network),
+      headers: getSnapshotNetworkHeaders(network)
     })
-    .then((response) => response.json())
+    .then(response => response.json())
     .catch(handleFailedRequest);
 };
 
@@ -45,10 +45,10 @@ export const fetchUserData = (address, type, timestamp, network) => {
         timestamp ? `&timestamp=${new Date(timestamp).toISOString()}` : ``
       }`,
       {
-        headers: getSnapshotNetworkHeaders(network),
+        headers: getSnapshotNetworkHeaders(network)
       }
     )
-    .then((response) => response.json())
+    .then(response => response.json())
     .catch(handleFailedRequest);
 };
 
@@ -57,18 +57,18 @@ export const fetchUserTimeSeriesData = (address, type, network) => {
     .fetch(
       `${getUrl(network)}/${type}?key=userTimeSeriesData&address=${address}`,
       {
-        headers: getSnapshotNetworkHeaders(network),
+        headers: getSnapshotNetworkHeaders(network)
       }
     )
-    .then((response) => response.json())
+    .then(response => response.json())
     .catch(handleFailedRequest);
 };
 
 export const fetchStack = (type, network) => {
   return window
     .fetch(`${getUrl(network)}/${type}?key=stack`, {
-      headers: getSnapshotNetworkHeaders(network),
+      headers: getSnapshotNetworkHeaders(network)
     })
-    .then((response) => response.json())
+    .then(response => response.json())
     .catch(handleFailedRequest);
 };
