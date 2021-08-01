f5cd71d5ed7140cc4f659472439aaf78b68036ca
McCall Alexander <mccallalexander96@gmail.com>
2021-08-01 12:53:46 -0600

    added v1 sunset scripts

--- js/scripts/get-all-user-data-at-v1-end.js
+++ js/scripts/get-all-user-data-at-v1-end.js
@@ -1,6 +1,15 @@
 const fetch = require('node-fetch').default;
 const moment = require('moment');
+
+require('fs').rmdirSync(
+  require('path').join(__dirname, `./output/get-all-user-data-at-v1-end`),
+  { recursive: true }
+);
+require('fs').mkdirSync(
+  require('path').join(__dirname, `./output/get-all-user-data-at-v1-end`)
+);
 const getAllUserDataAtV1End = async () => {
+  const startedAt = Date.now();
   const bigReqs = [];
   for (let type of ['lm', 'vs']) {
     const permaType = type;
@@ -9,8 +18,8 @@ const getAllUserDataAtV1End = async () => {
     const bigReq = fetch(
       `https://api-cryptoeconomics.sifchain.finance/api/${type}?key=users&snapshot-source=mainnet`
     )
-      .then(r => r.json())
-      .then(async data => {
+      .then((r) => r.json())
+      .then(async (data) => {
         let reqs = [];
         for (let u of data) {
           if (reqs.length > 20) {
@@ -20,17 +29,14 @@ const getAllUserDataAtV1End = async () => {
           const currI = ++i;
           reqs.push(
             (async () => {
-              while (!output[u]) {
+              while (true) {
                 try {
                   const timestamp = `${encodeURIComponent(
-                    moment('2021-08-04T23:59:59')
-                      .utc()
-                      .toISOString()
+                    moment('2021-08-04T23:59:59').utc().toISOString()
                   )}`;
-                  console.log({ timestamp });
                   const userData = await fetch(
                     `https://api-cryptoeconomics.sifchain.finance/api/${type}?key=userData&address=${u}&timestamp=${timestamp}`
-                  ).then(r => r.json());
+                  ).then((r) => r.json());
                   output[u] = true;
                   require('fs').writeFileSync(
                     require('path').join(
@@ -40,6 +46,16 @@ const getAllUserDataAtV1End = async () => {
                     JSON.stringify(userData)
                   );
                   console.log(`${currI} / ${data.length}`);
+                  const elapsed = Date.now() - startedAt;
+                  const estimatedTotalTime = (elapsed * data.length) / currI;
+                  console.log(
+                    `${~~(elapsed / 60000)} / ${~~(
+                      estimatedTotalTime / 60000
+                    )} minutes`
+                  );
+                  if (userData && userData.user) {
+                    break;
+                  }
                 } catch (e) {
                   console.error(e);
                 }
