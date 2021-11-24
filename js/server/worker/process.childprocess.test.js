c09933cfc0d888f21aad7b05157c3a8080214169
McCall Alexander <mccallalexander96@gmail.com>
2021-11-23 17:31:23 -0700

    added automated tests for expansion

--- js/server/worker/process.childprocess.test.js
+++ js/server/worker/process.childprocess.test.js
@@ -3,8 +3,10 @@ const _ = require('lodash');
 const { MAINNET } = require('../constants/snapshot-source-names');
 const { GET_LM_CURRENT_APY_SUMMARY } = require('../constants/action-names');
 const { getTimeIndex } = require('../util/getTimeIndex');
-const { encrypt, decrypt } = require('../util/encrypt');
 const fs = require('fs');
+const { encrypt, decrypt } = require('../util/encrypt');
+const configs = require('../config');
+const fetch = require('cross-fetch').fetch;
 
 if (process.env.DATABASE_URL) {
   process.env.DATABASE_URL = process.env.DATABASE_URL.replace(
@@ -55,23 +57,91 @@ const describe = async (description, describer) => {
   console.groupEnd();
 };
 
-const runTests = (type, parsedData, network, programName) => {
+const runTests = async (type, parsedData, network, programName) => {
+  const config = configs[programName];
+  const currentTimeIndex = getTimeIndex('now', programName);
   const finalGlobalTimestampState =
     parsedData.processedData[parsedData.processedData.length - 1];
-  const users = Object.values(finalGlobalTimestampState.users);
+  const currentGlobalTimestampState =
+    parsedData.processedData[currentTimeIndex];
 
-  const totalValuePerUser = Object.entries(
-    parsedData.processedData[getTimeIndex('now', programName)].users
-  ).reduce((prev, [addr, curr]) => {
-    if (!curr) return prev;
-    prev[addr] =
-      curr.totalAccruedCommissionsAndClaimableRewards +
-      curr.claimedCommissionsAndRewardsAwaitingDispensation +
-      curr.forfeitedCommissions +
-      curr.forfeited +
-      curr.dispensed;
-    return prev;
-  }, {});
+  const rankedAddresses = parsedData.users;
+  let addressIndexToCheck = 50;
+  const intervalsInADay = (24 * 60) / config.EVENT_INTERVAL_MINUTES;
+  const sampleStates = [
+    parsedData.processedData[currentTimeIndex],
+    parsedData.processedData[currentTimeIndex + intervalsInADay],
+  ];
+  const expectedDailyRate = config.STATIC_APR_PERCENTAGE / 365;
+  function checkCurrentPoolValueInRowan(address) {
+    return fetch(
+      `https://api.sifchain.finance/sifchain/clp/v1/liquidity_provider_data/${address}`
+    )
+      .then((r) => r.json())
+      .then((r) => {
+        return (
+          +r.liquidity_provider_data
+            .reduce((prev, curr) => {
+              if (
+                config.COIN_WHITELIST &&
+                !config.COIN_WHITELIST.includes(
+                  curr.liquidity_provider.asset.symbol
+                )
+              )
+                return prev;
+              return prev + BigInt(curr.native_asset_balance) * 2n;
+            }, 0n)
+            .toString() /
+          10 ** 18
+        );
+      });
+  }
+  while (addressIndexToCheck--) {
+    const address = rankedAddresses[addressIndexToCheck];
+    const sample1 = sampleStates[0].users[address];
+    const sample2 = sampleStates[1].users[address];
+    const rewardDelta =
+      sample2.totalAccruedCommissionsAndClaimableRewards -
+      sample1.totalAccruedCommissionsAndClaimableRewards;
+    if (!(sample1.totalDepositedAmount || sample2.totalDepositedAmount)) {
+      continue;
+    }
+    if (sample1.totalDepositedAmount !== sample2.totalDepositedAmount) continue;
+    const actualDailyRate = (rewardDelta / sample1.totalDepositedAmount) * 100;
+    // console.log(
+    //   address,
+    //   expectedDailyRate.toFixed(4) === actualDailyRate.toFixed(4),
+    //   expectedDailyRate,
+    //   actualDailyRate
+    // );
+    const expectedPoolValueInRowan = await checkCurrentPoolValueInRowan(
+      address
+    );
+    const actualPoolValueInRowan = sample1.totalDepositedAmount;
+    const diff = Math.abs(expectedPoolValueInRowan - actualPoolValueInRowan);
+    console.log(
+      address,
+      diff / expectedPoolValueInRowan,
+      expectedPoolValueInRowan,
+      actualPoolValueInRowan
+    );
+  }
+  const users = Object.values(currentGlobalTimestampState.users);
+
+  // const totalValuePerUser = Object.entries(
+  //   parsedData.processedData[
+  //     getTimeIndex('2021-10-15T17:26:13.441Z', programName)
+  //   ].users
+  // ).reduce((prev, [addr, curr]) => {
+  //   if (!curr) return prev;
+  //   prev[addr] =
+  //     curr.totalAccruedCommissionsAndClaimableRewards +
+  //     curr.claimedCommissionsAndRewardsAwaitingDispensation +
+  //     curr.forfeitedCommissions +
+  //     curr.forfeited +
+  //     curr.dispensed;
+  //   return prev;
+  // }, {});
 
   // require('fs').writeFileSync(
   //   './user-exit-states.with-readds.json',
@@ -107,7 +177,17 @@ const runTests = (type, parsedData, network, programName) => {
           curr.dispensed
         );
       }, 0);
+      const totalAccrued = users.reduce((prev, curr) => {
+        return (
+          prev + curr.totalAccruedCommissionsAndClaimableRewards //+
+          // curr.claimedCommissionsAndRewardsAwaitingDispensation //+
+          // curr.forfeitedCommissions +
+          // curr.forfeited +
+          // curr.dispensed
+        );
+      }, 0);
       log('totalRewards: ' + new Intl.NumberFormat().format(totalRewards));
+      log('totalAccrued: ' + new Intl.NumberFormat().format(totalAccrued));
       expect(Math.round(totalRewards) === 45000000);
     });
     test('totalPoolDominanceRatio', ({ expect, log }) => {
@@ -123,7 +203,8 @@ const runTests = (type, parsedData, network, programName) => {
 
 const bp = new BackgroundProcessor();
 // const bp2 = new BackgroundProcessor();
-const programName = 'harvest_expansion';
+// const programName = 'harvest_expansion';
+const programName = 'expansion_bonus';
 bp.reloadAndReprocessSnapshots({
   network: MAINNET,
   rewardProgram: programName,
